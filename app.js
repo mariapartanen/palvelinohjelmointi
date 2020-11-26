3747f761476c2d20120c110eba542b3102db66a4
Maria Partanen <mariapartanen@Maria-MacBook-Pro.local>
2020-11-26 17:51:32 +0200

    https://github.com/meijastiina/palvelinohjelmointiesimerkkeja/commit/75e89a93bba6f2a21c71ed02c4b3906eb2a33756

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hei taas');
+// Luo funktio tervehdi, joka ottaa parametrina nimen ja tulostaa tervehdyksen
+function tervehdi ( nimi ) {
+console.log( "Hei " + nimi );
+}
+let nimi = "Maria";
+// Kutsutaan funktiota 
+tervehdi( nimi );
