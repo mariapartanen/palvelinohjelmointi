9e18725731d3604a4cada574f6c07f4b926424fa
Maria Partanen <mariapartanen@Maria-MacBook-Pro.local>
2020-12-12 18:44:58 +0200

    staattisen tiedostojen hakemisto server.js tiedostoon

--- Projektityo/server.js
+++ Projektityo/server.js
@@ -1,3 +1,51 @@
+// Luodaan palvelin joka voi lähettää staattisia tiedostoja
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const lookup = require("mime-types").lookup;
+
+const server = http.createServer((req, res) => {
+    // käsittelee pyynnön ja lähetää takaisin staattiset tiedostot
+    //kansiosta `public`
+    let parsedURL = url.parse(req.url, true);
+    // poistetaan johtavat ja peräkkäiset kauttaviivat
+    let path = parsedURL.path.replace(/^\/+|\/+$/g, "");
+    /**
+     *  /
+     *  /projektityo.html
+     * 
+     *  /projektityo.css
+     *  /projektityo.js
+     */
+    if (path == "") {
+      path = "projektityo.html";
+    }
+    console.log(`Requested path ${path} `);
+
+    let file = __dirname + "/public/" + path;
+    // asynkronoitu read file käyttää callbackia
+    fs.readFile(file, function(err, content) {
+      if (err) {
+        console.log(`File Not Found ${File}`);
+        res.writeHead(404);
+        res.end();
+    } else { 
+      // Täsmentää sisältötyypin vastauksessa
+      console.log(`Returning ${path}`);
+      res.setHeader("X-Content-Type-Options", "nosniff");
+      let mime = lookup(path);
+      res.writeHead(200, { "Content-type": mime });
+      res.end(content);
+    }
+    });
+});
+
+server.listen(1234, "localhost", () => {
+    console.log("Listening on port 1234");
+});
+
+
 /* 
 const http = require('http')
 const fs = require('fs')
@@ -23,14 +71,14 @@ server.listen(port, function(error) {
         console.log('Server is listening on port ' + port)
     }
 })
-*/
+ */
 
 
-
+/* 
 var express = require("express");
  var app = express();
 
- app.get("/", function(req,res){
+ app.get("/", function(req, res){
      res.send("<h1>Hello Node</h1>");
  })
 
@@ -38,3 +86,4 @@ var express = require("express");
  app.listen(3000,function(){
      console.log("Listening to port 3000");
  })
+ */
