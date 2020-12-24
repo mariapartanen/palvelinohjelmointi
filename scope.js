457ec0a62565933b57c757875fd408a2be3a7cb2
mariapartanen <74195698+mariapartanen@users.noreply.github.com>
2020-12-24 17:10:22 +0200

    Delete scope.js

--- scope.js
+++
@@ -1,9 +0,0 @@
-let ekaMuuttuja = "Olen ekamuuttuja";
-console.log(ekaMuuttuja);
-let lippu = true;
-if ( lippu ) {
-    console.log(ekaMuuttuja);
-    tokaMuuttuja = "Olen tokamuuttuja";
-    console.log(tokaMuuttuja);
-}
-console.log(tokaMuuttuja);
