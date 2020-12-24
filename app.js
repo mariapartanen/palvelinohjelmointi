db19987fbfe74a208be820fa71aa859cb92da0b1
mariapartanen <74195698+mariapartanen@users.noreply.github.com>
2020-12-24 17:12:32 +0200

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// Luo funktio tervehdi, joka ottaa parametrina nimen ja tulostaa tervehdyksen
-function tervehdi ( nimi ) {
-console.log( "Hei " + nimi );
-}
-let nimi = "Maria";
-// Kutsutaan funktiota 
-tervehdi( nimi );
